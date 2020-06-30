d18e71f145c67fa6134333319194608931e94711
bravian barare <bravian@localhost.localdomain>
2020-06-30 05:34:06 +0300

    First commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import keys
+import time
+
+
+class TwitterBot:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.bot = webdriver.Firefox
+
+    def login(self):
+        bot = self.bot
+        bot.get('https://twitter.com/')
+        time.sleep(30)
+        email = bot.find_element_by_class_name('email-input')
+        password = bot.find_element_by_name('session[password]')
+        email.clear()
+        password.clear()
+        email.send_keys(self.username)
+        password.send_keys(self.password)
+
+ed = TwitterBot('bararebravian@gmail.com', '1661998brav')
+ed.login()
---
+++ twitterbot.py
@@ -0,0 +1,45 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import keys
+import time
+
+
+class TwitterBot:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.bot = webdriver.Firefox
+
+    def login(self):
+        bot = self.bot
+        bot.get('https://twitter.com/')
+        time.sleep(30)
+        email = bot.find_element_by_class_name('email-input')
+        password = bot.find_element_by_name('session[password]')
+        email.clear()
+        password.clear()
+        email.send_keys(self.username)
+        password.send_keys(self.password)
+        password.send_keys(keys.RETURN)
+        time.sleep(3)
+
+    def like_tweet(self, hashtag):
+        bot = self.bot
+        bot.get('https://twitter.com/search?q='  + hashtag + '&src=typeahead')
+        time.sleep(3)
+        for i in range(1,3):
+           bot.execute_script('window.scrollTo(0, document.body.scrollHeight) ')
+        time.sleep(2)
+        tweets = bot.find_element_by_class_name('tweet')
+        links = [elem.get_attribute('data-permalink-path') for elem in tweets]
+        print(links)
+        for link in links:
+            bot.get('http://twitter.com' + link)
+            try:
+                bot.find_element_by_class_name('HeartAnimation').click()
+                time.sleep(60)
+            except Exception as ex:
+                time.sleep(60)
+
+ed = TwitterBot('bararebravian@gmail.com', '1661998brav')
+ed.login()
+ed.like_tweet('KOT')
